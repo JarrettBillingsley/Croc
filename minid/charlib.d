2caffd8c7273be71e896fa835f7d2ff8067ede6f
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2007-04-04 01:00:49 +0000

    Added items remotely
    C:\dmd\proj\MiniD\minid\compiler.d
    C:\dmd\proj\MiniD\minid\iolib.d
    C:\dmd\proj\MiniD\minid\mathlib.d
    C:\dmd\proj\MiniD\minid\minid.d
    C:\dmd\proj\MiniD\minid\opcodes.d
    C:\dmd\proj\MiniD\minid\oslib.d
    C:\dmd\proj\MiniD\minid\regexplib.d
    C:\dmd\proj\MiniD\minid\stringlib.d
    C:\dmd\proj\MiniD\minid\tablelib.d
    C:\dmd\proj\MiniD\minid\types.d
    C:\dmd\proj\MiniD\minid\utils.d
    C:\dmd\proj\MiniD\minid\compile.bat
    C:\dmd\proj\MiniD\minid\run.bat
    C:\dmd\proj\MiniD\minid\arraylib.d
    C:\dmd\proj\MiniD\minid\baselib.d
    C:\dmd\proj\MiniD\minid\charlib.d

---
+++ minid/charlib.d
@@ -0,0 +1,131 @@
+/******************************************************************************
+License:
+Copyright (c) 2007 Jarrett Billingsley
+
+This software is provided 'as-is', without any express or implied warranty.
+In no event will the authors be held liable for any damages arising from the
+use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it freely,
+subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+	claim that you wrote the original software. If you use this software in a
+	product, an acknowledgment in the product documentation would be
+	appreciated but is not required.
+
+    2. Altered source versions must be plainly marked as such, and must not
+	be misrepresented as being the original software.
+
+    3. This notice may not be removed or altered from any source distribution.
+******************************************************************************/
+
+module minid.charlib;
+
+import minid.types;
+
+import ctype = std.ctype;
+import std.uni;
+
+class CharLib
+{
+	this(MDNamespace namespace)
+	{
+		namespace.addList
+		(
+			"toLower"d,    new MDClosure(namespace, &toLower,     "char.toLower"),
+			"toUpper"d,    new MDClosure(namespace, &toUpper,     "char.toUpper"),
+			"isAlpha"d,    new MDClosure(namespace, &isAlpha,     "char.isAlpha"),
+			"isAlNum"d,    new MDClosure(namespace, &isAlNum,     "char.isAlNum"),
+			"isLower"d,    new MDClosure(namespace, &isLower,     "char.isLower"),
+			"isUpper"d,    new MDClosure(namespace, &isUpper,     "char.isUpper"),
+			"isDigit"d,    new MDClosure(namespace, &isDigit,     "char.isDigit"),
+			"isCtrl"d,     new MDClosure(namespace, &isCtrl,      "char.isCtrl"),
+			"isPunct"d,    new MDClosure(namespace, &isPunct,     "char.isPunct"),
+			"isSpace"d,    new MDClosure(namespace, &isSpace,     "char.isSpace"),
+			"isHexDigit"d, new MDClosure(namespace, &isHexDigit,  "char.isHexDigit"),
+			"isAscii"d,    new MDClosure(namespace, &isAscii,     "char.isAscii")
+		);
+	}
+
+	int toLower(MDState s, uint numParams)
+	{
+		s.push(s.safeCode(toUniLower(s.getContext!(dchar))));
+		return 1;
+	}
+
+	int toUpper(MDState s, uint numParams)
+	{
+		s.push(s.safeCode(toUniUpper(s.getContext!(dchar))));
+		return 1;
+	}
+	
+	int isAlpha(MDState s, uint numParams)
+	{
+		s.push(cast(bool)isUniAlpha(s.getContext!(dchar)));
+		return 1;
+	}
+
+	int isAlNum(MDState s, uint numParams)
+	{
+		dchar c = s.getContext!(dchar);
+		s.push(cast(bool)(ctype.isdigit(c) || isUniAlpha(c)));
+		return 1;
+	}
+	
+	int isLower(MDState s, uint numParams)
+	{
+		s.push(cast(bool)isUniLower(s.getContext!(dchar)));
+		return 1;
+	}
+	
+	int isUpper(MDState s, uint numParams)
+	{
+		s.push(cast(bool)isUniUpper(s.getContext!(dchar)));
+		return 1;
+	}
+
+	int isDigit(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.isdigit(s.getContext!(dchar)));
+		return 1;
+	}
+
+	int isCtrl(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.iscntrl(s.getContext!(dchar)));
+		return 1;
+	}
+	
+	int isPunct(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.ispunct(s.getContext!(dchar)));
+		return 1;
+	}
+	
+	int isSpace(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.isspace(s.getContext!(dchar)));
+		return 1;
+	}
+	
+	int isHexDigit(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.isxdigit(s.getContext!(dchar)));
+		return 1;
+	}
+	
+	int isAscii(MDState s, uint numParams)
+	{
+		s.push(cast(bool)ctype.isascii(s.getContext!(dchar)));
+		return 1;
+	}
+}
+
+public void init()
+{
+	MDNamespace namespace = new MDNamespace("char"d, MDGlobalState().globals);
+	new CharLib(namespace);
+	MDGlobalState().setMetatable(MDValue.Type.Char, namespace);
+}
