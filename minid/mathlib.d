4523f72072de81e27ff92eca496db3c68d4fcc2e
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2008-08-23 23:14:26 +0000

    mathlib is done.

--- minid/mathlib.d
+++ minid/mathlib.d
@@ -25,8 +25,8 @@ module minid.mathlib;
 
 import math = tango.math.Math;
 import tango.math.GammaFunction;
-import tango.math.Random;
 import tango.math.IEEE;
+import tango.math.random.Kiss;
 
 import minid.ex;
 import minid.interpreter;
@@ -55,35 +55,35 @@ static:
 			pushFloat(t, mdfloat.min);      newGlobal(t, "floatMin");
 			pushFloat(t, mdfloat.max);      newGlobal(t, "floatMax");
 
-// 			newFunction(t, &abs, "abs");       newGlobal(t, "abs");
-// 			newFunction(t, &sin, "sin");       newGlobal(t, "sin");
-// 			newFunction(t, &cos, "cos");       newGlobal(t, "cos");
-// 			newFunction(t, &tan, "tan");       newGlobal(t, "tan");
-// 			newFunction(t, &asin, "asin");     newGlobal(t, "asin");
-// 			newFunction(t, &acos, "acos");     newGlobal(t, "acos");
-// 			newFunction(t, &atan, "atan");     newGlobal(t, "atan");
-// 			newFunction(t, &atan2, "atan2");   newGlobal(t, "atan2");
-// 			newFunction(t, &sqrt, "sqrt");     newGlobal(t, "sqrt");
-// 			newFunction(t, &cbrt, "cbrt");     newGlobal(t, "cbrt");
-// 			newFunction(t, &pow, "pow");       newGlobal(t, "pow");
-// 			newFunction(t, &exp, "exp");       newGlobal(t, "exp");
-// 			newFunction(t, &ln, "ln");         newGlobal(t, "ln");
-// 			newFunction(t, &log2, "log2");     newGlobal(t, "log2");
-// 			newFunction(t, &log10, "log10");   newGlobal(t, "log10");
-// 			newFunction(t, &hypot, "hypot");   newGlobal(t, "hypot");
-// 			newFunction(t, &lgamma, "lgamma"); newGlobal(t, "lgamma");
-// 			newFunction(t, &gamma, "gamma");   newGlobal(t, "gamma");
-// 			newFunction(t, &ceil, "ceil");     newGlobal(t, "ceil");
-// 			newFunction(t, &floor, "floor");   newGlobal(t, "floor");
-// 			newFunction(t, &round, "round");   newGlobal(t, "round");
-// 			newFunction(t, &trunc, "trunc");   newGlobal(t, "trunc");
-// 			newFunction(t, &isNan, "isNan");   newGlobal(t, "isNan");
-// 			newFunction(t, &isInf, "isInf");   newGlobal(t, "isInf");
-// 			newFunction(t, &sign, "sign");     newGlobal(t, "sign");
-// 			newFunction(t, &rand, "rand");     newGlobal(t, "rand");
-// 			newFunction(t, &frand, "frand");   newGlobal(t, "frand");
-// 			newFunction(t, &max, "max");       newGlobal(t, "max");
-// 			newFunction(t, &min, "min");       newGlobal(t, "min");
+			newFunction(t, &abs, "abs");       newGlobal(t, "abs");
+			newFunction(t, &sin, "sin");       newGlobal(t, "sin");
+			newFunction(t, &cos, "cos");       newGlobal(t, "cos");
+			newFunction(t, &tan, "tan");       newGlobal(t, "tan");
+			newFunction(t, &asin, "asin");     newGlobal(t, "asin");
+			newFunction(t, &acos, "acos");     newGlobal(t, "acos");
+			newFunction(t, &atan, "atan");     newGlobal(t, "atan");
+			newFunction(t, &atan2, "atan2");   newGlobal(t, "atan2");
+			newFunction(t, &sqrt, "sqrt");     newGlobal(t, "sqrt");
+			newFunction(t, &cbrt, "cbrt");     newGlobal(t, "cbrt");
+			newFunction(t, &pow, "pow");       newGlobal(t, "pow");
+			newFunction(t, &exp, "exp");       newGlobal(t, "exp");
+			newFunction(t, &ln, "ln");         newGlobal(t, "ln");
+			newFunction(t, &log2, "log2");     newGlobal(t, "log2");
+			newFunction(t, &log10, "log10");   newGlobal(t, "log10");
+			newFunction(t, &hypot, "hypot");   newGlobal(t, "hypot");
+			newFunction(t, &lgamma, "lgamma"); newGlobal(t, "lgamma");
+			newFunction(t, &gamma, "gamma");   newGlobal(t, "gamma");
+			newFunction(t, &ceil, "ceil");     newGlobal(t, "ceil");
+			newFunction(t, &floor, "floor");   newGlobal(t, "floor");
+			newFunction(t, &round, "round");   newGlobal(t, "round");
+			newFunction(t, &trunc, "trunc");   newGlobal(t, "trunc");
+			newFunction(t, &isNan, "isNan");   newGlobal(t, "isNan");
+			newFunction(t, &isInf, "isInf");   newGlobal(t, "isInf");
+			newFunction(t, &sign, "sign");     newGlobal(t, "sign");
+			newFunction(t, &rand, "rand");     newGlobal(t, "rand");
+			newFunction(t, &frand, "frand");   newGlobal(t, "frand");
+			newFunction(t, &max, "max");       newGlobal(t, "max");
+			newFunction(t, &min, "min");       newGlobal(t, "min");
 
 			return 0;
 		}, "math");
@@ -94,323 +94,329 @@ static:
 		importModule(t, "math");
 	}
 
-/+
-	mdfloat getFloat(MDState s, uint i)
+	mdfloat get_float(MDThread* t, word i)
 	{
-		if(s.isParam!("int")(i))
-			return s.getParam!(int)(i);
-		else if(s.isParam!("float")(i))
-			return s.getParam!(mdfloat)(i);
+		if(isInt(t, i))
+			return getInt(t, i);
+		else if(isFloat(t, i))
+			return getFloat(t, i);
 		else
-			s.throwRuntimeException("Expected 'int' or 'float', not '{}'", s.getParam(i).typeString());
-	}
-
-	int abs(MDState s, uint numParams)
-	{
-		if(s.isParam!("int")(0))
-			s.push(math.abs(s.getParam!(int)(0)));
-		else if(s.isParam!("float")(0))
-			s.push(math.abs(s.getParam!(mdfloat)(0)));
-		else
-			s.throwRuntimeException("Expected 'int' or 'float', not '{}'", s.getParam(0u).typeString());
-
-		return 1;
-	}
-	
-	int sin(MDState s, uint numParams)
-	{
-		s.push(math.sin(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int cos(MDState s, uint numParams)
-	{
-		s.push(math.cos(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int tan(MDState s, uint numParams)
-	{
-		s.push(math.tan(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int asin(MDState s, uint numParams)
-	{
-		s.push(math.asin(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int acos(MDState s, uint numParams)
-	{
-		s.push(math.acos(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int atan(MDState s, uint numParams)
-	{
-		s.push(math.atan(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int atan2(MDState s, uint numParams)
-	{
-		s.push(math.atan2(getFloat(s, 0), getFloat(s, 1)));
-		return 1;
-	}
-	
-	int sqrt(MDState s, uint numParams)
-	{
-		s.push(math.sqrt(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int cbrt(MDState s, uint numParams)
-	{
-		s.push(math.cbrt(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int exp(MDState s, uint numParams)
-	{
-		s.push(math.exp(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int ln(MDState s, uint numParams)
-	{
-		s.push(math.log(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int log2(MDState s, uint numParams)
-	{
-		s.push(math.log2(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int log10(MDState s, uint numParams)
-	{
-		s.push(math.log10(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int hypot(MDState s, uint numParams)
-	{
-		s.push(math.hypot(getFloat(s, 0), getFloat(s, 1)));
-		return 1;
-	}
-	
-	int lgamma(MDState s, uint numParams)
-	{
-		s.push(logGamma(getFloat(s, 0)));
-		return 1;
-	}
-
-	int gamma(MDState s, uint numParams)
-	{
-		s.push(.gamma(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int ceil(MDState s, uint numParams)
-	{
-		s.push(math.ceil(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int floor(MDState s, uint numParams)
-	{
-		s.push(math.floor(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int round(MDState s, uint numParams)
-	{
-		s.push(cast(int)math.round(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int trunc(MDState s, uint numParams)
-	{
-		s.push(cast(int)math.trunc(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int isNan(MDState s, uint numParams)
-	{
-		s.push(cast(bool)math.isNaN(getFloat(s, 0)));
-		return 1;
-	}
-
-	int isInf(MDState s, uint numParams)
-	{
-		s.push(cast(bool)math.isInfinity(getFloat(s, 0)));
-		return 1;
-	}
-	
-	int sign(MDState s, uint numParams)
-	{
-		if(s.isParam!("int")(0))
 		{
-			int val = s.getParam!(int)(0);
+			pushTypeString(t, i);
+			throwException(t, "Expected 'int' or 'float', not '{}'", getString(t, -1));
+		}
+		
+		assert(false);
+	}
+
+	uword abs(MDThread* t, uword numParams)
+	{
+		if(isInt(t, 1))
+			pushInt(t, math.abs(getInt(t, 1)));
+		else if(isFloat(t, 1))
+			pushFloat(t, math.abs(getFloat(t, 1)));
+		else
+		{
+			pushTypeString(t, 1);
+			throwException(t, "Expected 'int' or 'float', not '{}'", getString(t, -1));
+		}
+
+		return 1;
+	}
+
+	uword sin(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.sin(get_float(t, 1)));
+		return 1;
+	}
+
+	uword cos(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.cos(get_float(t, 1)));
+		return 1;
+	}
+
+	uword tan(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.tan(get_float(t, 1)));
+		return 1;
+	}
+
+	uword asin(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.asin(get_float(t, 1)));
+		return 1;
+	}
+
+	uword acos(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.acos(get_float(t, 1)));
+		return 1;
+	}
+
+	uword atan(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.atan(get_float(t, 1)));
+		return 1;
+	}
+
+	uword atan2(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.atan2(get_float(t, 1), get_float(t, 2)));
+		return 1;
+	}
+
+	uword sqrt(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.sqrt(get_float(t, 1)));
+		return 1;
+	}
+
+	uword cbrt(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.cbrt(get_float(t, 1)));
+		return 1;
+	}
+
+	uword exp(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.exp(get_float(t, 1)));
+		return 1;
+	}
+
+	uword ln(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.log(get_float(t, 1)));
+		return 1;
+	}
+
+	uword log2(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.log2(get_float(t, 1)));
+		return 1;
+	}
+
+	uword log10(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.log10(get_float(t, 1)));
+		return 1;
+	}
+
+	uword hypot(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.hypot(get_float(t, 1), get_float(t, 2)));
+		return 1;
+	}
+
+	uword lgamma(MDThread* t, uword numParams)
+	{
+		pushFloat(t, logGamma(get_float(t, 1)));
+		return 1;
+	}
+
+	uword gamma(MDThread* t, uword numParams)
+	{
+		pushFloat(t, .gamma(get_float(t, 1)));
+		return 1;
+	}
+
+	uword ceil(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.ceil(get_float(t, 1)));
+		return 1;
+	}
+
+	uword floor(MDThread* t, uword numParams)
+	{
+		pushFloat(t, math.floor(get_float(t, 1)));
+		return 1;
+	}
+
+	uword round(MDThread* t, uword numParams)
+	{
+		pushInt(t, cast(int)math.round(get_float(t, 1)));
+		return 1;
+	}
+
+	uword trunc(MDThread* t, uword numParams)
+	{
+		pushInt(t, cast(int)math.trunc(get_float(t, 1)));
+		return 1;
+	}
+
+	uword isNan(MDThread* t, uword numParams)
+	{
+		pushBool(t, cast(bool)math.isNaN(get_float(t, 1)));
+		return 1;
+	}
+
+	uword isInf(MDThread* t, uword numParams)
+	{
+		pushBool(t, cast(bool)math.isInfinity(get_float(t, 1)));
+		return 1;
+	}
+
+	uword sign(MDThread* t, uword numParams)
+	{
+		if(isInt(t, 1))
+		{
+			auto val = getInt(t, 1);
 
 			if(val < 0)
-				s.push(-1);
+				pushInt(t, -1);
 			else if(val > 0)
-				s.push(1);
+				pushInt(t, 1);
 			else
-				s.push(0);
+				pushInt(t, 0);
 		}
 		else
 		{
-			mdfloat val = s.getParam!(mdfloat)(0);
+			auto val = getFloat(t, 1);
 
 			if(val < 0)
-				s.push(-1);
+				pushInt(t, -1);
 			else if(val > 0)
-				s.push(1);
+				pushInt(t, 1);
 			else
-				s.push(0);
+				pushInt(t, 0);
 		}
 		
 		return 1;
 	}
 	
-	int pow(MDState s, uint numParams)
+	uword pow(MDThread* t, uword numParams)
 	{
-		mdfloat base = getFloat(s, 0);
-		
-		if(s.isParam!("int")(1))
-			s.push(math.pow(cast(real)base, s.getParam!(int)(1)));
+		auto base = get_float(t, 1);
+
+		if(isInt(t, 2))
+			pushFloat(t, math.pow(cast(real)base, getInt(t, 2)));
 		else
-			s.push(math.pow(base, getFloat(s, 1)));
-			
+			pushFloat(t, math.pow(base, get_float(t, 2)));
+
 		return 1;
 	}
 
-	int rand(MDState s, uint numParams)
+	uword rand(MDThread* t, uword numParams)
 	{
-		uint num = Random.shared.next();
+		// uint is the return type of Kiss.toInt
+		static if(uint.sizeof < mdint.sizeof)
+		{
+			mdint num = Kiss.shared.toInt();
+			num |= (cast(ulong)Kiss.shared.toInt()) << 32;
+		}
+		else
+			mdint num = Kiss.shared.toInt();
 
 		switch(numParams)
 		{
 			case 0:
-				s.push(cast(int)num);
+				pushInt(t, num);
 				break;
 
 			case 1:
-				auto max = s.getParam!(int)(0);
+				auto max = getInt(t, 1);
 
 				if(max == 0)
-					s.throwRuntimeException("Maximum value may not be 0");
+					throwException(t, "Maximum value may not be 0");
 
-				s.push(cast(uint)num % max);
+				pushInt(t, cast(uword)num % max);
 				break;
 
 			default:
-				int lo = s.getParam!(int)(0);
-				int hi = s.getParam!(int)(1);
-				
+				auto lo = getInt(t, 1);
+				auto hi = getInt(t, 2);
+
 				if(hi == lo)
-					s.throwRuntimeException("Low and high values must be different");
+					throwException(t, "Low and high values must be different");
 
-				s.push(cast(int)(num % (hi - lo)) + lo);
+				pushInt(t, cast(mdint)(num % (hi - lo)) + lo);
 				break;
 		}
-		
+
 		return 1;
 	}
-	
-	int frand(MDState s, uint numParams)
+
+	uword frand(MDThread* t, uword numParams)
 	{
-		auto num = cast(mdfloat)Random.shared.next() / uint.max;
+		auto num = cast(mdfloat)Kiss.shared.toInt() / uint.max;
 
 		switch(numParams)
 		{
 			case 0:
-				s.push(num);
+				pushFloat(t, num);
 				break;
 
 			case 1:
-				s.push(num * s.getParam!(mdfloat)(0));
+				pushFloat(t, num * getFloat(t, 1));
 				break;
 
 			default:
-				auto lo = s.getParam!(mdfloat)(0);
-				auto hi = s.getParam!(mdfloat)(1);
+				auto lo = getFloat(t, 1);
+				auto hi = getFloat(t, 2);
 
-				s.push((num * (hi - lo)) + lo);
+				pushFloat(t, (num * (hi - lo)) + lo);
 				break;
 		}
-		
+
 		return 1;
 	}
-	
-	int max(MDState s, uint numParams)
+
+	uword max(MDThread* t, uword numParams)
 	{
 		switch(numParams)
 		{
 			case 0:
-				s.throwRuntimeException("At least one parameter required");
-				
-			case 1:
-				break;
-				
-			case 2:
-				if(s.cmp(s.getParam(0u), s.getParam(1u)) > 0)
-					s.pop();
-				break;
-
-			default:
-				auto m = s.getParam(0u);
-
-				for(uint i = 1; i < numParams; i++)
-				{
-					auto v = s.getParam(i);
-
-					if(s.cmp(v, m) > 0)
-						m = v;
-				}
-				
-				s.push(m);
-				break;
-		}
-		
-		return 1;
-	}
-	
-	int min(MDState s, uint numParams)
-	{
-		switch(numParams)
-		{
-			case 0:
-				s.throwRuntimeException("At least one parameter required");
+				throwException(t, "At least one parameter required");
 
 			case 1:
 				break;
 
 			case 2:
-				if(s.cmp(s.getParam(0u), s.getParam(1u)) < 0)
-					s.pop();
+				if(cmp(t, 1, 2) > 0)
+					pop(t);
 				break;
 
 			default:
-				auto m = s.getParam(0u);
+				word m = 1;
 
-				for(uint i = 1; i < numParams; i++)
-				{
-					auto v = s.getParam(i);
+				for(uword i = 2; i <= numParams; i++)
+					if(cmp(t, i, m) > 0)
+						m = i;
 
-					if(s.cmp(v, m) < 0)
-						m = v;
-				}
-
-				s.push(m);
+				dup(t, m);
 				break;
 		}
 
 		return 1;
 	}
-+/
+
+	uword min(MDThread* t, uword numParams)
+	{
+		switch(numParams)
+		{
+			case 0:
+				throwException(t, "At least one parameter required");
+
+			case 1:
+				break;
+
+			case 2:
+				if(cmp(t, 1, 2) < 0)
+					pop(t);
+				break;
+
+			default:
+				word m = 1;
+
+				for(uword i = 2; i <= numParams; i++)
+					if(cmp(t, i, m) < 0)
+						m = i;
+
+				dup(t, m);
+				break;
+		}
+
+
+		return 1;
+	}
 }
