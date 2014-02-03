f46b76f4cc76e438a09c30596a7a8dbf3e917cb7
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2014-02-02 23:05:56 -0800

    Ported the funcdef APIs.

---
+++ src/croc/api/funcdef.cpp
@@ -0,0 +1,26 @@
+
+#include "croc/api.h"
+#include "croc/internal/apichecks.hpp"
+#include "croc/internal/stack.hpp"
+#include "croc/types.hpp"
+
+namespace croc
+{
+extern "C"
+{
+	const char* croc_funcdef_getName(CrocThread* t_, word_t funcdef)
+	{
+		auto t = Thread::from(t_);
+		API_CHECK_PARAM(fd, funcdef, Funcdef, "funcdef");
+		return fd->name->toCString();
+	}
+
+	const char* croc_funcdef_getNamen(CrocThread* t_, word_t funcdef, uword_t* len)
+	{
+		auto t = Thread::from(t_);
+		API_CHECK_PARAM(fd, funcdef, Funcdef, "funcdef");
+		*len = fd->name->length;
+		return fd->name->toCString();
+	}
+}
+}
