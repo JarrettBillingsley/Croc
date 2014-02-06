8873a072d9c98820f7826d39703460a14d909e71
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2014-02-05 20:22:20 -0800

    Ported the raw memory APIs.

---
+++ src/croc/api/mem.cpp
@@ -0,0 +1,41 @@
+
+#include "croc/api.h"
+#include "croc/internal/stack.hpp"
+#include "croc/types.hpp"
+
+namespace croc
+{
+extern "C"
+{
+	void* croc_mem_alloc(CrocThread* t_, uword_t size)
+	{
+		auto t = Thread::from(t_);
+		return DArray<char>::alloc(t->vm->mem, size).ptr;
+	}
+
+	void croc_mem_resize(CrocThread* t_, void** mem, uword_t* memSize, uword_t newSize)
+	{
+		auto t = Thread::from(t_);
+		auto arr = DArray<char>::n(cast(char*)*mem, *memSize);
+		arr.resize(t->vm->mem, newSize);
+		*mem = arr.ptr;
+		*memSize = arr.length;
+	}
+
+	void* croc_mem_dup(CrocThread* t_, void* mem, uword_t memSize)
+	{
+		auto t = Thread::from(t_);
+		auto arr = DArray<char>::n(cast(char*)mem, memSize);
+		return arr.dup(t->vm->mem).ptr;
+	}
+
+	void croc_mem_free(CrocThread* t_, void** mem, uword_t* memSize)
+	{
+		auto t = Thread::from(t_);
+		auto arr = DArray<char>::n(cast(char*)*mem, *memSize);
+		arr.free(t->vm->mem);
+		*mem = nullptr;
+		*memSize = 0;
+	}
+}
+}
