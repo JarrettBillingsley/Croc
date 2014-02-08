a557820fa81594c895f471fb36bb4a295bc41c05
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2014-02-08 14:51:10 -0800

    I really need to stop git pushing before I know that the commit is good.

--- croctest.cpp
+++ croctest.cpp
@@ -22,6 +22,10 @@ word_t println(CrocThread* t)
 int main()
 {
 	auto t = croc_vm_open(&croc_DefaultMemFunc, nullptr);
+	// croc_vm_loadUnsafeLibs(t, CrocUnsafeLib_ReallyAll);
+	// croc_vm_loadAvailableAddons(t);
+	// croc_compiler_setFlags(t, CrocCompilerFlags_All | CrocCompilerFlags_DocDecorators);
+	// runModule(t, "samples.simple");
 
 	croc_function_new(t, "println", -1, &println, 0);
 	croc_newGlobal(t, "println");
@@ -31,8 +35,23 @@ int main()
 
 	croc_function_newScript(t, -1);
 	croc_pushNull(t);
-	croc_call(t, -2, 0);
+	auto result = croc_tryCall(t, -2, 0);
+
+	if(result < 0)
+	{
+		printf("------------ ERROR ------------\n");
+		croc_pushToString(t, -1);
+		printf("%s\n", croc_getString(t, -1));
+		croc_popTop(t);
+
+		croc_dupTop(t);
+		croc_pushNull(t);
+		croc_methodCall(t, -2, "tracebackString", 1);
+		printf("%s\n", croc_getString(t, -1));
+
+		croc_pop(t, 2);
+	}
 
 	croc_vm_close(t);
-	return 0;
+	fflush(stdout);
 }
