3877b011771ef01c75daa795b6b2ef2b9f2cf72c
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2014-07-10 17:45:20 -0700

    Envlib converted

--- src/croc/stdlib/env.cpp
+++ src/croc/stdlib/env.cpp
@@ -9,9 +9,10 @@
 
 namespace croc
 {
-	namespace
-	{
-DBeginList(_globalFuncs)
+namespace
+{
+const _StdlibRegisterInfo _getVar_info =
+{
 	Docstr(DFunc("getVar") DParam("name", "string") DParamD("defaultVal", "string", "null")
 	R"(Get the value of the environment variable named \tt{name}.
 
@@ -22,35 +23,43 @@ DBeginList(_globalFuncs)
 	\returns the value of the environment variable, if any. If no variable named \tt{name} exists, it will return
 		\tt{defaultVal} instead (which defaults to \tt{null}).)"),
 
-	"getVar", 2, [](CrocThread* t) -> word_t
+	"getVar", 2
+};
+
+word_t _getVar(CrocThread* t)
+{
+	croc_ex_checkStringParam(t, 1);
+	auto haveDef = croc_ex_optParam(t, 2, CrocType_String);
+	auto name = getCrocstr(t, 1);
+
+	if(!oscompat::getEnv(t, name))
 	{
-		croc_ex_checkStringParam(t, 1);
-		auto haveDef = croc_ex_optParam(t, 2, CrocType_String);
-		auto name = getCrocstr(t, 1);
-
-		if(!oscompat::getEnv(t, name))
-		{
-			if(haveDef)
-				croc_dup(t, 2);
-			else
-				croc_pushNull(t);
-		}
-
-		return 1;
+		if(haveDef)
+			croc_dup(t, 2);
+		else
+			croc_pushNull(t);
 	}
 
-DListSep()
+	return 1;
+}
+
+const _StdlibRegisterInfo _getAllVars_info =
+{
 	Docstr(DFunc("getAllVars")
 	R"(\returns a table containing all variables in this process's environment. The keys are the names and the values
 	are... the values.)"),
 
-	"getAllVars", 0, [](CrocThread* t) -> word_t
-	{
-		oscompat::getAllEnvVars(t);
-		return 1;
-	}
+	"getAllVars", 0
+};
 
-DListSep()
+word_t _getAllVars(CrocThread* t)
+{
+	oscompat::getAllEnvVars(t);
+	return 1;
+}
+
+const _StdlibRegisterInfo _setVar_info =
+{
 	Docstr(DFunc("setVar") DParam("name", "string") DParamD("val", "string", "null")
 	R"(Set or unset a variable in this process's environment.
 
@@ -58,42 +67,52 @@ DListSep()
 	\param[val] is the value to set the variable to. If you set the variable to the empty string or \tt{null}, the
 		variable will be unset (removed from the process's environment).)"),
 
-	"setVar", 2, [](CrocThread* t) -> word_t
-	{
-		croc_ex_checkStringParam(t, 1);
-		auto haveVal = croc_ex_optParam(t, 2, CrocType_String);
-		auto name = getCrocstr(t, 1);
+	"setVar", 2
+};
 
-		if(haveVal)
-			oscompat::setEnv(t, name, getCrocstr(t, 2));
-		else
-			oscompat::setEnv(t, name, crocstr());
+word_t _setVar(CrocThread* t)
+{
+	croc_ex_checkStringParam(t, 1);
+	auto haveVal = croc_ex_optParam(t, 2, CrocType_String);
+	auto name = getCrocstr(t, 1);
 
-		return 0;
-	}
-DEndList()
+	if(haveVal)
+		oscompat::setEnv(t, name, getCrocstr(t, 2));
+	else
+		oscompat::setEnv(t, name, crocstr());
 
-	word loader(CrocThread* t)
-	{
-		registerGlobals(t, _globalFuncs);
-		return 0;
-	}
-	}
+	return 0;
+}
 
-	void initEnvLib(CrocThread* t)
-	{
-		croc_ex_makeModule(t, "env", &loader);
-		croc_ex_importNS(t, "env");
+const _StdlibRegister _globalFuncs[] =
+{
+	_DListItem(_getVar),
+	_DListItem(_getAllVars),
+	_DListItem(_setVar),
+	_DListEnd
+};
+
+word loader(CrocThread* t)
+{
+	_registerGlobals(t, _globalFuncs);
+	return 0;
+}
+}
+
+void initEnvLib(CrocThread* t)
+{
+	croc_ex_makeModule(t, "env", &loader);
+	croc_ex_importNS(t, "env");
 #ifdef CROC_BUILTIN_DOCS
-		CrocDoc doc;
-		croc_ex_doc_init(t, &doc, __FILE__);
-		croc_ex_doc_push(&doc,
-		DModule("env")
-		R"(This small module allows you to get and set process environment variables.)");
-			docFields(&doc, _globalFuncs);
-		croc_ex_doc_pop(&doc, -1);
-		croc_ex_doc_finish(&doc);
+	CrocDoc doc;
+	croc_ex_doc_init(t, &doc, __FILE__);
+	croc_ex_doc_push(&doc,
+	DModule("env")
+	R"(This small module allows you to get and set process environment variables.)");
+		_docFields(&doc, _globalFuncs);
+	croc_ex_doc_pop(&doc, -1);
+	croc_ex_doc_finish(&doc);
 #endif
-		croc_popTop(t);
-	}
+	croc_popTop(t);
+}
 }
