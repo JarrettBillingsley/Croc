e5cd09fe221cc716b1d4c4d296cc713a33ea1d68
Jarrett Billingsley <jarrett.billingsley@gmail.com>
2014-07-10 23:18:25 -0700

    Timelib converted.

--- src/croc/stdlib/time.cpp
+++ src/croc/stdlib/time.cpp
@@ -9,55 +9,55 @@
 
 namespace croc
 {
-	namespace
+namespace
+{
+void _DateTimeToTable(CrocThread* t, oscompat::DateTime time, word dest)
+{
+	croc_pushInt(t, time.year);  croc_fielda(t, dest, "year");
+	croc_pushInt(t, time.month); croc_fielda(t, dest, "month");
+	croc_pushInt(t, time.day);   croc_fielda(t, dest, "day");
+	croc_pushInt(t, time.hour);  croc_fielda(t, dest, "hour");
+	croc_pushInt(t, time.min);   croc_fielda(t, dest, "min");
+	croc_pushInt(t, time.sec);   croc_fielda(t, dest, "sec");
+	croc_pushInt(t, time.msec);  croc_fielda(t, dest, "msec");
+}
+
+void _TableToDateTime(CrocThread* t, word src, oscompat::DateTime& time)
+{
+	auto year =  croc_field(t, src, "year");
+	auto month = croc_field(t, src, "month");
+	auto day =   croc_field(t, src, "day");
+	auto hour =  croc_field(t, src, "hour");
+	auto min =   croc_field(t, src, "min");
+	auto sec =   croc_field(t, src, "sec");
+	auto msec =  croc_field(t, src, "msec");
+
+	if(!croc_isInt(t, year) || !croc_isInt(t, month) || !croc_isInt(t, day))
+		croc_eh_throwStd(t, "ValueError",
+			"year, month, and day fields in time table must exist and must be integers");
+
+	time.year = cast(uint16_t)croc_getInt(t, year);
+	time.month = cast(uint16_t)croc_getInt(t, month);
+	time.day = cast(uint16_t)croc_getInt(t, day);
+
+	if(croc_isInt(t, hour) && croc_isInt(t, min) && croc_isInt(t, sec))
 	{
-	void _DateTimeToTable(CrocThread* t, oscompat::DateTime time, word dest)
+		time.hour = cast(uint16_t)croc_getInt(t, hour);
+		time.min = cast(uint16_t)croc_getInt(t, min);
+		time.sec = cast(uint16_t)croc_getInt(t, sec);
+	}
+	else
 	{
-		croc_pushInt(t, time.year);  croc_fielda(t, dest, "year");
-		croc_pushInt(t, time.month); croc_fielda(t, dest, "month");
-		croc_pushInt(t, time.day);   croc_fielda(t, dest, "day");
-		croc_pushInt(t, time.hour);  croc_fielda(t, dest, "hour");
-		croc_pushInt(t, time.min);   croc_fielda(t, dest, "min");
-		croc_pushInt(t, time.sec);   croc_fielda(t, dest, "sec");
-		croc_pushInt(t, time.msec);  croc_fielda(t, dest, "msec");
+		time.hour = time.min = time.sec = 0;
 	}
 
-	void _TableToDateTime(CrocThread* t, word src, oscompat::DateTime& time)
-	{
-		auto year =  croc_field(t, src, "year");
-		auto month = croc_field(t, src, "month");
-		auto day =   croc_field(t, src, "day");
-		auto hour =  croc_field(t, src, "hour");
-		auto min =   croc_field(t, src, "min");
-		auto sec =   croc_field(t, src, "sec");
-		auto msec =  croc_field(t, src, "msec");
+	if(croc_isInt(t, msec))
+		time.msec = cast(uint16_t)croc_getInt(t, msec);
+	else
+		time.msec = 0;
 
-		if(!croc_isInt(t, year) || !croc_isInt(t, month) || !croc_isInt(t, day))
-			croc_eh_throwStd(t, "ValueError",
-				"year, month, and day fields in time table must exist and must be integers");
-
-		time.year = cast(uint16_t)croc_getInt(t, year);
-		time.month = cast(uint16_t)croc_getInt(t, month);
-		time.day = cast(uint16_t)croc_getInt(t, day);
-
-		if(croc_isInt(t, hour) && croc_isInt(t, min) && croc_isInt(t, sec))
-		{
-			time.hour = cast(uint16_t)croc_getInt(t, hour);
-			time.min = cast(uint16_t)croc_getInt(t, min);
-			time.sec = cast(uint16_t)croc_getInt(t, sec);
-		}
-		else
-		{
-			time.hour = time.min = time.sec = 0;
-		}
-
-		if(croc_isInt(t, msec))
-			time.msec = cast(uint16_t)croc_getInt(t, msec);
-		else
-			time.msec = 0;
-
-		croc_pop(t, 7);
-	}
+	croc_pop(t, 7);
+}
 
 #ifdef CROC_BUILTIN_DOCS
 const char* ModuleDocs =
@@ -71,7 +71,8 @@ international time and date formatting etc. These things would be much better de
 internationalization library.)";
 #endif
 
-DBeginList(_globalFuncs)
+const _StdlibRegisterInfo _microTime_info =
+{
 	Docstr(DFunc("microTime")
 	R"(\returns an integer which is a microseond-accurate count of some kind. The values returned by this function can
 	be used to accurately measure how long something takes, like so:
@@ -89,31 +90,39 @@ writefln("Took {} milliseconds", (end - start) / 1000.0)
 
 	\see \link{Timer} for a simple class that wraps this in a simpler interface.)"),
 
-	"microTime", 0, [](CrocThread* t) -> word_t
-	{
-		croc_pushInt(t, cast(crocint)oscompat::microTime());
-		return 1;
-	}
+	"microTime", 0
+};
 
-DListSep()
+word_t _microTime(CrocThread* t)
+{
+	croc_pushInt(t, cast(crocint)oscompat::microTime());
+	return 1;
+}
+
+const _StdlibRegisterInfo _timex_info =
+{
 	Docstr(DFunc("timex") DParamAny("f")
 	R"(Given any callable value \tt{f}, calls \tt{f} and measures how long it takes to complete.
 
 	\returns an integer representing the elapsed time in microseconds.)"),
 
-	"timex", 1, [](CrocThread* t) -> word_t
-	{
-		croc_ex_checkAnyParam(t, 1);
-		croc_dup(t, 1);
-		croc_pushNull(t);
-		auto start = oscompat::microTime();
-		croc_call(t, -2, 0);
-		auto end = oscompat::microTime();
-		croc_pushInt(t, cast(crocint)(end - start));
-		return 1;
-	}
+	"timex", 1
+};
 
-DListSep()
+word_t _timex(CrocThread* t)
+{
+	croc_ex_checkAnyParam(t, 1);
+	croc_dup(t, 1);
+	croc_pushNull(t);
+	auto start = oscompat::microTime();
+	croc_call(t, -2, 0);
+	auto end = oscompat::microTime();
+	croc_pushInt(t, cast(crocint)(end - start));
+	return 1;
+}
+
+const _StdlibRegisterInfo _clockTime_info =
+{
 	Docstr(DFunc("clockTime")
 	R"(\returns an integer which is a count of the number of microseconds since the Unix epoch (1 Jan 1970).
 	\b{Important:} just because this function returns microseconds does not mean it is actually accurate to the
@@ -125,13 +134,17 @@ DListSep()
 	To convert a raw clock time into separate date and time components (month, day, hours, minutes etc.) see
 	\link{timeToTableLocal} and \link{timeToTableUTC}.)"),
 
-	"clockTime", 0, [](CrocThread* t) -> word_t
-	{
-		croc_pushInt(t, cast(crocint)oscompat::sysTime());
-		return 1;
-	}
+	"clockTime", 0
+};
 
-DListSep()
+word_t _clockTime(CrocThread* t)
+{
+	croc_pushInt(t, cast(crocint)oscompat::sysTime());
+	return 1;
+}
+
+const _StdlibRegisterInfo _timeToTableUTC_info =
+{
 	Docstr(DFunc("timeToTableUTC") DParamD("time", "int", "null") DParamD("ret", "table", "null")
 	R"(Converts a clock time (such as returned from \link{clockTime}) into a table which holds the the date and time
 	according to the Gregorian calendar. The time components given will be in UTC time. For local time, see
@@ -154,30 +167,38 @@ DListSep()
 		\li{\tt{"msec"}}: the milliseconds, 0-999.
 	\endlist)"),
 
-	"timeToTableUTC", 1, [](CrocThread* t) -> word_t
-	{
-		auto time = croc_ex_optParam(t, 1, CrocType_Int) ? cast(oscompat::Time)croc_getInt(t, 1) : oscompat::sysTime();
-		auto ret = croc_ex_optParam(t, 2, CrocType_Table) ? 2 : croc_table_new(t, 0);
-		_DateTimeToTable(t, oscompat::timeToDateTime(time, false), ret);
-		croc_dup(t, ret);
-		return 1;
-	}
+	"timeToTableUTC", 1
+};
 
-DListSep()
+word_t _timeToTableUTC(CrocThread* t)
+{
+	auto time = croc_ex_optParam(t, 1, CrocType_Int) ? cast(oscompat::Time)croc_getInt(t, 1) : oscompat::sysTime();
+	auto ret = croc_ex_optParam(t, 2, CrocType_Table) ? 2 : croc_table_new(t, 0);
+	_DateTimeToTable(t, oscompat::timeToDateTime(time, false), ret);
+	croc_dup(t, ret);
+	return 1;
+}
+
+const _StdlibRegisterInfo _timeToTableLocal_info =
+{
 	Docstr(DFunc("timeToTableLocal") DParamD("time", "int", "null") DParamD("ret", "table", "null")
 	R"(Just like \link{timeToTableUTC}, except the fields in the returned table will be calculated according to the
 	local timezone instead of UTC.)"),
 
-	"timeToTableLocal", 1, [](CrocThread* t) -> word_t
-	{
-		auto time = croc_ex_optParam(t, 1, CrocType_Int) ? cast(oscompat::Time)croc_getInt(t, 1) : oscompat::sysTime();
-		auto ret = croc_ex_optParam(t, 2, CrocType_Table) ? 2 : croc_table_new(t, 0);
-		_DateTimeToTable(t, oscompat::timeToDateTime(time, true), ret);
-		croc_dup(t, ret);
-		return 1;
-	}
+	"timeToTableLocal", 1
+};
 
-DListSep()
+word_t _timeToTableLocal(CrocThread* t)
+{
+	auto time = croc_ex_optParam(t, 1, CrocType_Int) ? cast(oscompat::Time)croc_getInt(t, 1) : oscompat::sysTime();
+	auto ret = croc_ex_optParam(t, 2, CrocType_Table) ? 2 : croc_table_new(t, 0);
+	_DateTimeToTable(t, oscompat::timeToDateTime(time, true), ret);
+	croc_dup(t, ret);
+	return 1;
+}
+
+const _StdlibRegisterInfo _timeFromTableUTC_info =
+{
 	Docstr(DFunc("timeFromTableUTC") DParam("tab", "table")
 	R"(The inverse of \link{timeToTableUTC}, converts a table with the appropriate fields into a integer clock time
 	value. The time components of the table are interpreted as being in UTC.
@@ -198,29 +219,47 @@ DListSep()
 
 	\throws[ValueError] if \tt{tab} is invalid in any way.)"),
 
-	"timeFromTableUTC", 1, [](CrocThread* t) -> word_t
-	{
-		croc_ex_checkParam(t, 1, CrocType_Table);
-		oscompat::DateTime dt;
-		_TableToDateTime(t, 1, dt);
-		croc_pushInt(t, cast(crocint)oscompat::dateTimeToTime(dt, false));
-		return 1;
-	}
+	"timeFromTableUTC", 1
+};
 
-DListSep()
+word_t _timeFromTableUTC(CrocThread* t)
+{
+	croc_ex_checkParam(t, 1, CrocType_Table);
+	oscompat::DateTime dt;
+	_TableToDateTime(t, 1, dt);
+	croc_pushInt(t, cast(crocint)oscompat::dateTimeToTime(dt, false));
+	return 1;
+}
+
+const _StdlibRegisterInfo _timeFromTableLocal_info =
+{
 	Docstr(DFunc("timeFromTableLocal") DParam("tab", "table")
 	R"(The inverse of \link{timeToTableLocal}. This works just like \link{timeFromTableUTC} except it interprets the
 	time components of \tt{tab} as local time instead of UTC.)"),
 
-	"timeFromTableLocal", 1, [](CrocThread* t) -> word_t
-	{
-		croc_ex_checkParam(t, 1, CrocType_Table);
-		oscompat::DateTime dt;
-		_TableToDateTime(t, 1, dt);
-		croc_pushInt(t, cast(crocint)oscompat::dateTimeToTime(dt, true));
-		return 1;
-	}
-DEndList()
+	"timeFromTableLocal", 1
+};
+
+word_t _timeFromTableLocal(CrocThread* t)
+{
+	croc_ex_checkParam(t, 1, CrocType_Table);
+	oscompat::DateTime dt;
+	_TableToDateTime(t, 1, dt);
+	croc_pushInt(t, cast(crocint)oscompat::dateTimeToTime(dt, true));
+	return 1;
+}
+
+const _StdlibRegister _globalFuncs[] =
+{
+	_DListItem(_microTime),
+	_DListItem(_timex),
+	_DListItem(_clockTime),
+	_DListItem(_timeToTableUTC),
+	_DListItem(_timeToTableLocal),
+	_DListItem(_timeFromTableUTC),
+	_DListItem(_timeFromTableLocal),
+	_DListEnd
+};
 
 #ifdef CROC_BUILTIN_DOCS
 const char* TimerClassDocs =
@@ -248,120 +287,144 @@ writeln(t.time()) // prints 0
 \endcode)";
 #endif
 
-DBeginList(_TimerMethodFuncs)
+const _StdlibRegisterInfo _start_info =
+{
 	Docstr(DFunc("start")
 	R"(Starts the timer, or if it's already running, does nothing.)"),
 
-	"start", 0, [](CrocThread* t) -> word_t
+	"start", 0
+};
+
+word_t _start(CrocThread* t)
+{
+	croc_field(t, 0, "_running");
+
+	if(!croc_isTrue(t, -1))
 	{
-		croc_field(t, 0, "_running");
-
-		if(!croc_isTrue(t, -1))
-		{
-			croc_pushBool(t, true);
-			croc_fielda(t, 0, "_running");
-			croc_pushInt(t, cast(crocint)oscompat::microTime());
-			croc_fielda(t, 0, "_start");
-		}
-
-		return 0;
+		croc_pushBool(t, true);
+		croc_fielda(t, 0, "_running");
+		croc_pushInt(t, cast(crocint)oscompat::microTime());
+		croc_fielda(t, 0, "_start");
 	}
 
-DListSep()
+	return 0;
+}
+
+const _StdlibRegisterInfo _stop_info =
+{
 	Docstr(DFunc("stop")
 	R"(Stops the timer, or if it's not running, does nothing.)"),
 
-	"stop", 0, [](CrocThread* t) -> word_t
-	{
-		auto end = cast(crocint)oscompat::microTime();
-		croc_field(t, 0, "_running");
+	"stop", 0
+};
 
-		if(croc_isTrue(t, -1))
-		{
-			croc_pushBool(t, false);
-			croc_fielda(t, 0, "_running");
-			croc_field(t, 0, "_start");
-			croc_field(t, 0, "_total");
-			croc_pushInt(t, (end - croc_getInt(t, -2)) + croc_getInt(t, -1));
-			croc_fielda(t, 0, "_total");
-		}
+word_t _stop(CrocThread* t)
+{
+	auto end = cast(crocint)oscompat::microTime();
+	croc_field(t, 0, "_running");
 
-		return 0;
-	}
-
-DListSep()
-	Docstr(DFunc("reset")
-	R"(Stops the timer and resets its accumulated time to 0.)"),
-
-	"reset", 0, [](CrocThread* t) -> word_t
+	if(croc_isTrue(t, -1))
 	{
 		croc_pushBool(t, false);
 		croc_fielda(t, 0, "_running");
-		croc_pushInt(t, 0);
+		croc_field(t, 0, "_start");
+		croc_field(t, 0, "_total");
+		croc_pushInt(t, (end - croc_getInt(t, -2)) + croc_getInt(t, -1));
 		croc_fielda(t, 0, "_total");
-		return 0;
 	}
 
-DListSep()
+	return 0;
+}
+
+const _StdlibRegisterInfo _reset_info =
+{
+	Docstr(DFunc("reset")
+	R"(Stops the timer and resets its accumulated time to 0.)"),
+
+	"reset", 0
+};
+
+word_t _reset(CrocThread* t)
+{
+	croc_pushBool(t, false);
+	croc_fielda(t, 0, "_running");
+	croc_pushInt(t, 0);
+	croc_fielda(t, 0, "_total");
+	return 0;
+}
+
+const _StdlibRegisterInfo _time_info =
+{
 	Docstr(DFunc("time")
 	R"(\returns the total time elapsed on this timer. If this timer is running, returns the accumulated time plus any
 	time since the last call to \link{start}. If this timer is stopped, just returns the total accumulated time.)"),
 
-	"time", 0, [](CrocThread* t) -> word_t
+	"time", 0
+};
+
+word_t _time(CrocThread* t)
+{
+	auto now = cast(crocint)oscompat::microTime();
+
+	croc_field(t, 0, "_running");
+
+	if(croc_isTrue(t, -1))
 	{
-		auto now = cast(crocint)oscompat::microTime();
-
-		croc_field(t, 0, "_running");
-
-		if(croc_isTrue(t, -1))
-		{
-			croc_field(t, 0, "_start");
-			croc_field(t, 0, "_total");
-			croc_pushInt(t, (now - croc_getInt(t, -2)) + croc_getInt(t, -1));
-		}
-		else
-		{
-			croc_field(t, 0, "_total");
-		}
-
-		return 1;
+		croc_field(t, 0, "_start");
+		croc_field(t, 0, "_total");
+		croc_pushInt(t, (now - croc_getInt(t, -2)) + croc_getInt(t, -1));
 	}
-DEndList()
-
-	word loader(CrocThread* t)
+	else
 	{
-		registerGlobals(t, _globalFuncs);
-
-		croc_class_new(t, "Timer", 0);
-			croc_pushBool(t, false); croc_class_addField(t, -2, "_running");
-			croc_pushInt(t, 0);      croc_class_addField(t, -2, "_start");
-			croc_pushInt(t, 0);      croc_class_addField(t, -2, "_total");
-			registerMethods(t, _TimerMethodFuncs);
-		croc_newGlobal(t, "Timer");
-
-		return 0;
-	}
+		croc_field(t, 0, "_total");
 	}
 
-	void initTimeLib(CrocThread* t)
-	{
-		oscompat::initTime();
-		croc_ex_makeModule(t, "time", &loader);
-		croc_ex_importNS(t, "time");
+	return 1;
+}
+
+const _StdlibRegister _Timer_methods[] =
+{
+	_DListItem(_start),
+	_DListItem(_stop),
+	_DListItem(_reset),
+	_DListItem(_time),
+	_DListEnd
+};
+
+word loader(CrocThread* t)
+{
+	_registerGlobals(t, _globalFuncs);
+
+	croc_class_new(t, "Timer", 0);
+		croc_pushBool(t, false); croc_class_addField(t, -2, "_running");
+		croc_pushInt(t, 0);      croc_class_addField(t, -2, "_start");
+		croc_pushInt(t, 0);      croc_class_addField(t, -2, "_total");
+		_registerMethods(t, _Timer_methods);
+	croc_newGlobal(t, "Timer");
+
+	return 0;
+}
+}
+
+void initTimeLib(CrocThread* t)
+{
+	oscompat::initTime();
+	croc_ex_makeModule(t, "time", &loader);
+	croc_ex_importNS(t, "time");
 #ifdef CROC_BUILTIN_DOCS
-		CrocDoc doc;
-		croc_ex_doc_init(t, &doc, __FILE__);
-		croc_ex_doc_push(&doc, ModuleDocs);
-			docFields(&doc, _globalFuncs);
+	CrocDoc doc;
+	croc_ex_doc_init(t, &doc, __FILE__);
+	croc_ex_doc_push(&doc, ModuleDocs);
+		_docFields(&doc, _globalFuncs);
 
-			croc_field(t, -1, "Timer");
-			croc_ex_doc_push(&doc, TimerClassDocs);
-				docFields(&doc, _TimerMethodFuncs);
-			croc_ex_doc_pop(&doc, -1);
-			croc_popTop(t);
+		croc_field(t, -1, "Timer");
+		croc_ex_doc_push(&doc, TimerClassDocs);
+			_docFields(&doc, _Timer_methods);
 		croc_ex_doc_pop(&doc, -1);
-		croc_ex_doc_finish(&doc);
-#endif
 		croc_popTop(t);
-	}
+	croc_ex_doc_pop(&doc, -1);
+	croc_ex_doc_finish(&doc);
+#endif
+	croc_popTop(t);
+}
 }
